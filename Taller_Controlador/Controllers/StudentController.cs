2fd3e0ae5c93a3fed9272b217a84bd17bffe89e6
D Macias <PIERINEX05@gmail.com>
2023-07-27 20:09:54 -0500

    Agregar archivos de proyecto.

---
+++ Taller_Controlador/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Taller_Controlador.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Controlador de estudiantes";
+        }
+    }
+}
